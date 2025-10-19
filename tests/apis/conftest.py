53fb60b502a64005fa5dd0c6a263a15a80cc2657
anghoo <leeanghoo2@gmail.com>
2025-10-19 22:09:32 +0900

    chore: setup initial project environment (#1)

--- tests/apis/conftest.py
+++ tests/apis/conftest.py
@@ -1,5 +1,5 @@
 import pytest_asyncio
-from httpx import AsyncClient
+from httpx import AsyncClient, ASGITransport
 from sqlmodel.ext.asyncio.session import AsyncSession
 
 from src.database import close_db, create_db_and_tables, engine
@@ -8,10 +8,16 @@ from src.main import app
 
 @pytest_asyncio.fixture(scope="function")
 async def client() -> AsyncClient:
-    async with AsyncClient(app=app, base_url="http://127.0.0.1:8000") as client:
-        await create_db_and_tables()
+    await create_db_and_tables()
+
+    transport = ASGITransport(app=app)
+
+    async with AsyncClient(
+        transport=transport, base_url="http://127.0.0.1:8000"
+    ) as client:
         yield client
-        await close_db()
+
+    await close_db()
 
 
 @pytest_asyncio.fixture(scope="function")
