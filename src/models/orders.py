111ed0adfd28869e44dd5a59fbef6c36c76a4191
anghoo <leeanghoo2@gmail.com>
2025-11-02 09:42:44 +0900

    feat: add order_status table (#2)

--- src/models/orders.py
+++ src/models/orders.py
@@ -13,15 +13,16 @@ if TYPE_CHECKING:
 
 class Orders(Base):
     __tablename__ = "orders"
-
+    # Columns
     order_id: Mapped[int] = mapped_column(
         BigInteger, primary_key=True, autoincrement=True
     )
     member_id: Mapped[int] = mapped_column(
-        ForeignKey("member.member_id"), nullable=False
+        BigInteger, ForeignKey("member.member_id"), nullable=False
     )
-    member: Mapped["Member"] = relationship("Member", back_populates="orders")
-    order_status: Mapped[str] = mapped_column(String(50), nullable=False)
+    order_status_id: Mapped[int] = mapped_column(
+        BigInteger, ForeignKey("order_status.order_status_id"), nullable=False
+    )
     ordered_at: Mapped[datetime] = mapped_column(DateTime, nullable=False)
     created_at: Mapped[datetime] = mapped_column(
         DateTime, nullable=False, default=func.now()
@@ -33,23 +34,28 @@ class Orders(Base):
     deleted_at: Mapped[datetime | None] = mapped_column(
         DateTime, nullable=True, default=None
     )
+    # Relationship
     order_product: Mapped[list["OrderProduct"]] = relationship(
         "OrderProduct", back_populates="orders"
     )
+    order_status: Mapped["OrderStatus"] = relationship(
+        "OrderStatus", back_populates="orders"
+    )
+    member: Mapped["Member"] = relationship("Member", back_populates="orders")
 
 
 class OrderProduct(Base):
     __tablename__ = "order_product"
-
+    # Columns
     order_product_id: Mapped[int] = mapped_column(
         BigInteger, primary_key=True, autoincrement=True
     )
-    order_id: Mapped[int] = mapped_column(ForeignKey("orders.order_id"), nullable=False)
-    order: Mapped["Orders"] = relationship("Orders", back_populates="order_product")
-    product_id: Mapped[int] = mapped_column(
-        ForeignKey("product.product_id"), nullable=False
+    order_id: Mapped[int] = mapped_column(
+        BigInteger, ForeignKey("orders.order_id"), nullable=False
     )
-    product: Mapped["Product"] = relationship("Product", back_populates="order_product")
+    product_id: Mapped[int] = mapped_column(
+        BigInteger, ForeignKey("product.product_id"), nullable=False
+    )
     order_product_price: Mapped[int] = mapped_column(Integer, nullable=False)
     order_product_quantity: Mapped[int] = mapped_column(Integer, nullable=False)
     created_at: Mapped[datetime] = mapped_column(
@@ -61,4 +67,26 @@ class OrderProduct(Base):
     is_deleted: Mapped[bool] = mapped_column(Boolean, nullable=False, default=False)
     deleted_at: Mapped[datetime | None] = mapped_column(
         DateTime, nullable=True, default=None
+    )
+    # Relationship
+    order: Mapped["Orders"] = relationship("Orders", back_populates="order_product")
+    product: Mapped["Product"] = relationship("Product", back_populates="order_product")
+
+
+class OrderStatus(Base):
+    __tablename__ = "order_status"
+    # Columns
+    order_status_id: Mapped[int] = mapped_column(
+        BigInteger, primary_key=True, autoincrement=True
+    )
+    order_status_category: Mapped[str] = mapped_column(String(50), nullable=False)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime, nullable=False, default=func.now()
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime, nullable=False, default=func.now(), onupdate=func.now()
+    )
+    # Relationship
+    order: Mapped[list["Orders"]] = relationship(
+        "Orders", back_populates="order_status"
     )
